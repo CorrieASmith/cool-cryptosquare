ae56a35e90e3dcf8d8ed567627a7fe2637d36a19
Corrie Smith & Michael Summers <student@epicodus.com>
2015-09-08 12:13:57 -0700

    removes white space from given string

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var coolCryptosquare = function (sentence) {
+  var count = 0;
+  sentence = sentence.replace(/\s/g, '');
+  return sentence;
+};
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('coolCryptosquare', function () {
+  it("removes the spaces from a given string", function () {
+    expect(coolCryptosquare("I like turtles")).to.equal("Iliketurtles");
+  });
+});
