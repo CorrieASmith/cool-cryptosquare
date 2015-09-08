871e8f34564cc1f78b2347ca451f50539f4cb39f
Corrie Smith & Michael Summers <student@epicodus.com>
2015-09-08 12:17:55 -0700

    downspaces entire input string. #spec 1 fixed to still pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var coolCryptosquare = function (sentence) {
   var count = 0;
+  sentence = sentence.toLowerCase();
   sentence = sentence.replace(/\s/g, '');
   return sentence;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
 describe('coolCryptosquare', function () {
   it("removes the spaces from a given string", function () {
-    expect(coolCryptosquare("I like turtles")).to.equal("Iliketurtles");
+    expect(coolCryptosquare("I like turtles")).to.equal("iliketurtles");
+  });
+
+  it("downcases all characters in a given string", function () {
+    expect(coolCryptosquare("I like turtles")).to.equal("iliketurtles");
   });
 });
