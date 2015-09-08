c68404e23f4965abf269c8600978ffc6bbb9121d
Corrie Smith & Michael Summers <student@epicodus.com>
2015-09-08 13:16:12 -0700

    findSquare method and spec.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 var coolCryptosquare = function (sentence) {
-  var count = 0;
   sentence = sentence.toLowerCase();
   sentence = sentence.replace(/\s/g, '');
   return sentence;
+};
+
+var findSquare = function(strLength) {
+  var count = 1;
+  while ((count * count) < strLength) {
+    count += 1;
+  };
+  return count;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -7,3 +7,8 @@ describe('coolCryptosquare', function () {
     expect(coolCryptosquare("I like turtles")).to.equal("iliketurtles");
   });
 });
+describe('findSquare', function() {
+  it("attempts to find the proper square for the input val", function () {
+    expect(findSquare(12)).to.equal(4); //The closest square is 4x4=16, looking for the 4.
+  });
+});
